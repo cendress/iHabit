0149aabd8232a8784afcc9a313884b24c909c8f2
cendress <95492688+cendress@users.noreply.github.com>
2024-07-04 09:26:38 -0400

    Create methods in Habits that will be able to load and save habit data

--- iHabit/Habits.swift
+++ iHabit/Habits.swift
@@ -8,9 +8,31 @@
 import SwiftUI
 
 class Habits: ObservableObject {
-    @Published var habits = [Habit]()
+    @Published var habits = [Habit]() {
+        didSet {
+            saveHabits()
+        }
+    }
+    
+    init() {
+        loadHabits()
+    }
     
     func addHabit(_ habit: Habit) {
         habits.append(habit)
+    }
+    
+    private func saveHabits() {
+        if let encoded = try? JSONEncoder().encode(habits) {
+            UserDefaults.standard.setValue(encoded, forKey: "Habits")
+        }
+    }
+    
+    private func loadHabits() {
+        if let savedHabits = UserDefaults.standard.object(forKey: "Habits") as? Data {
+            if let decodedHabits = try? JSONDecoder().decode([Habit].self, from: savedHabits) {
+                habits = decodedHabits
+            }
+        }
     }
 }
