d657380bdd0e562e5c92fb7b2c4f436ce508e7e6
cendress <95492688+cendress@users.noreply.github.com>
2024-07-04 08:14:06 -0400

    Initial Commit

---
+++ iHabit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iHabit
+//
+//  Created by Christopher Endress on 7/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iHabit/iHabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  iHabitApp.swift
+//  iHabit
+//
+//  Created by Christopher Endress on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct iHabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
